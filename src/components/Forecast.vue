ea3ff867784553b42424e77e8c234841c8a2b689
Bryan Barrows <bryan.barrows@seattlecolleges.edu>
2018-02-26 20:50:19 -0800

    implement caching of API requests on Forecast.vue

--- src/components/Forecast.vue
+++ src/components/Forecast.vue
@@ -46,33 +46,37 @@ export default {
   },
   created () {
     this.showLoading = true;
+
+    let cacheLabel = 'forecast_' + this.$route.params.cityId;
+    let cacheExpiry = 15 * 60 * 1000; // 15 minute expiration
     
-    // TODO: Cache these API results using the City ID as the label
-
-    // TODO: Create a cacheLabel value
-
-    // TODO: Create a cacheExpiry value set to 15 minutes in milliseconds
-
     // TODO: Use a conditional to check if the API query has been cached
     // If so, use that cached data
     // If not, make the API call and cache the data with the cacheLabel and cacheExpiry defined above
-
-    API.get('forecast', {
+    if (this.$ls.get(cacheLabel)){
+      console.log('Cached query detected.');
+      this.weatherData = this.$ls.get(cacheLabel);
+      this.showLoading = false;
+    } else {
+      console.log('No cache detected. Making API request.');
+      API.get('forecast', {
       params: {
           id: this.$route.params.cityId
-      }
-    })
-    .then(response => {
-      this.showLoading = false;
-      this.weatherData = response.data;
-    })
-    .catch(error => {
-      this.showLoading = false;
-      this.messages.push({
-        type: 'error',
-        text: error.message
+        }
+      })
+      .then(response => {
+        this.$ls.set(cacheLabel, response.data, cacheExpiry);
+        this.showLoading = false;
+        this.weatherData = response.data;
+      })
+      .catch(error => {
+        this.showLoading = false;
+        this.messages.push({
+          type: 'error',
+          text: error.message
+        });
       });
-    });
+    }
   },
   filters: {
     formatDate: function (timestamp){
